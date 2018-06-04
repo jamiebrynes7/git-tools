781e4be3f6d7e4f68342c3272977d2583944ff1e
Jamie Brynes <jamiebrynes7@gmail.com>
2018-06-05 00:27:30 +0100

    lint files

--- src/bin/branch_selector.rs
+++ src/bin/branch_selector.rs
@@ -1,5 +1,5 @@
 extern crate git_shared;
-use git_shared::{ GitBranch, error_and_exit, run_git_command, get_list_branches };
+use git_shared::{error_and_exit, get_list_branches, run_git_command, GitBranch};
 
 use std::io::{self, Write};
 use std::process;
@@ -7,16 +7,22 @@ use std::process;
 enum UserInputResult {
     Invalid(String),
     Valid(usize),
-    Exit
+    Exit,
 }
 
 fn main() {
     let branch_list = get_list_branches();
     let desired_branch_index = select_branch_index(&branch_list) - 1;
 
-    let switch_branch_command = run_git_command(&vec!["checkout".to_string(), branch_list[desired_branch_index].name.clone() ]);
+    let switch_branch_command = run_git_command(&vec![
+        "checkout".to_string(),
+        branch_list[desired_branch_index].name.clone(),
+    ]);
     if switch_branch_command.code != 0 {
-        error_and_exit(format!("Switching branches failed with:\n{}", switch_branch_command.stderr))
+        error_and_exit(format!(
+            "Switching branches failed with:\n{}",
+            switch_branch_command.stderr
+        ))
     }
 }
 
@@ -32,7 +38,7 @@ fn select_branch_index(branches: &Vec<GitBranch>) -> usize {
         match get_user_input(branches.len()) {
             UserInputResult::Exit => process::exit(0),
             UserInputResult::Invalid(s) => println!("{}", s),
-            UserInputResult::Valid(i) => return i
+            UserInputResult::Valid(i) => return i,
         }
     }
 }
@@ -41,23 +47,21 @@ fn get_user_input(max_value: usize) -> UserInputResult {
     print!("\n> ");
     io::stdout().flush().unwrap();
     let mut user_input = String::new();
-    io::stdin().read_line(&mut user_input)
+    io::stdin()
+        .read_line(&mut user_input)
         .expect("Failed to read from stdin.");
 
     match user_input.trim() {
         "q" => UserInputResult::Exit,
-        x => {
-            match x.parse::<usize>() {
-                Ok(i) => {
-                    if i > max_value || i == 0 {
-                        UserInputResult::Invalid("Given branch index does not exist.".to_string())
-                    }
-                    else {
-                        UserInputResult::Valid(i)
-                    }
-                },
-                Err(_) => UserInputResult::Invalid("Invalid input!".to_string())
+        x => match x.parse::<usize>() {
+            Ok(i) => {
+                if i > max_value || i == 0 {
+                    UserInputResult::Invalid("Given branch index does not exist.".to_string())
+                } else {
+                    UserInputResult::Valid(i)
+                }
             }
-        }
+            Err(_) => UserInputResult::Invalid("Invalid input!".to_string()),
+        },
     }
 }
--- src/bin/clean_branches.rs
+++ src/bin/clean_branches.rs
@@ -1,16 +1,22 @@
 extern crate git_shared;
-use git_shared::{ GitBranch, ProcessOutput, error_and_exit, run_git_command, get_list_branches, get_pruned_branches };
+use git_shared::{error_and_exit, get_list_branches, get_pruned_branches, run_git_command,
+                 GitBranch, ProcessOutput};
 
 use std::io::{self, Write};
 
 fn main() {
-    let git_prune_origin_command = run_git_command(&vec!["remote".to_string(),
-                                                         "prune".to_string(),
-                                                         "origin".to_string(),
-                                                         "--dry-run".to_string()]);
+    let git_prune_origin_command = run_git_command(&vec![
+        "remote".to_string(),
+        "prune".to_string(),
+        "origin".to_string(),
+        "--dry-run".to_string(),
+    ]);
 
     if git_prune_origin_command.code != 0 {
-        error_and_exit(format!("git prune remote origin failed with:\n{}", git_prune_origin_command.stderr));
+        error_and_exit(format!(
+            "git prune remote origin failed with:\n{}",
+            git_prune_origin_command.stderr
+        ));
     }
 
     let pruned_branch_list = get_pruned_branches(git_prune_origin_command.stdout);
@@ -19,25 +25,36 @@ fn main() {
 
     if branches_to_delete.len() == 0 {
         println!("Found no branches to delete!");
-        return
+        return;
     }
 
     match get_user_confirmation(&branches_to_delete, &pruned_branch_list) {
         true => delete_branches(&branches_to_delete),
-        false => println!("Aborting operation!")
+        false => println!("Aborting operation!"),
     }
 }
 
-fn get_intersection(remote_branches: &Vec<GitBranch>, local_branches: &Vec<GitBranch>) -> Vec<GitBranch> {
-
-    let intersection = local_branches.iter().cloned()
-        .filter(|branch| remote_branches.iter().any(|remote_branch| branch.name == remote_branch.name))
+fn get_intersection(
+    remote_branches: &Vec<GitBranch>,
+    local_branches: &Vec<GitBranch>,
+) -> Vec<GitBranch> {
+    let intersection = local_branches
+        .iter()
+        .cloned()
+        .filter(|branch| {
+            remote_branches
+                .iter()
+                .any(|remote_branch| branch.name == remote_branch.name)
+        })
         .collect();
 
     return intersection;
 }
 
-fn get_user_confirmation(branches_to_delete: &Vec<GitBranch>, remote_branches: &Vec<GitBranch>) -> bool {
+fn get_user_confirmation(
+    branches_to_delete: &Vec<GitBranch>,
+    remote_branches: &Vec<GitBranch>,
+) -> bool {
     println!("This will delete the following local and remote branches:");
     for branch in branches_to_delete {
         println!(" * {}", branch);
@@ -49,25 +66,39 @@ fn get_user_confirmation(branches_to_delete: &Vec<GitBranch>, remote_branches: &
     print!("\nEnter y to confirm: ");
     io::stdout().flush().unwrap();
     let mut user_input = String::new();
-    io::stdin().read_line(&mut user_input)
+    io::stdin()
+        .read_line(&mut user_input)
         .expect("Failed to read from stdin.");
 
     match user_input.trim().to_lowercase().as_str() {
         "y" => true,
-        _ => false
+        _ => false,
     }
 }
 
 fn delete_branches(branches_to_delete: &Vec<GitBranch>) {
     for branch in branches_to_delete {
-        match run_git_command(&vec!["branch".to_string(), "-D".to_string(), branch.name.clone()]) {
-            ProcessOutput{code, ..} if code == 0 => println!("Deleting: {}", branch),
-            ProcessOutput{stderr, ..} => error_and_exit(format!("Failed to delete branch: {} with error: {}", branch, stderr)),
+        match run_git_command(&vec![
+            "branch".to_string(),
+            "-D".to_string(),
+            branch.name.clone(),
+        ]) {
+            ProcessOutput { code, .. } if code == 0 => println!("Deleting: {}", branch),
+            ProcessOutput { stderr, .. } => error_and_exit(format!(
+                "Failed to delete branch: {} with error: {}",
+                branch, stderr
+            )),
         };
     }
 
-    match run_git_command(&vec!["remote".to_string(), "prune".to_string(), "origin".to_string()]) {
-        ProcessOutput{code, ..} if code == 0 => println!("Deleting remotes..."),
-        ProcessOutput{stderr, ..} => error_and_exit(format!("Failed to delete remotes with error: {}", stderr)),
+    match run_git_command(&vec![
+        "remote".to_string(),
+        "prune".to_string(),
+        "origin".to_string(),
+    ]) {
+        ProcessOutput { code, .. } if code == 0 => println!("Deleting remotes..."),
+        ProcessOutput { stderr, .. } => {
+            error_and_exit(format!("Failed to delete remotes with error: {}", stderr))
+        }
     };
 }

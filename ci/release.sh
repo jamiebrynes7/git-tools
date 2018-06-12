1ce4eeb00a7eb0b5de8e83b2e222f4284101b664
Jamie Brynes <jamiebrynes7@gmail.com>
2018-06-12 23:56:58 +0100

    fix release script

--- ci/release.sh
+++ ci/release.sh
@@ -7,10 +7,7 @@ PATCH_VERSION=0
 
 VERSION="v${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}"
 
-echo "Creating releases for version ${VERSION}"
+echo "Creating releases for version ${VERSION} and ${TARGET}"
 
 mkdir -p build
-
-zip -j build/git-tools-$(VERSION)-x86_64-linux-gnu.zip target/x86_64-unknown-linux-gnu/release/branch_selector target/x86_64-unknown-linux-gnu/release/clean_branches
-
-zip -j build/git-tools-$(VERSION)-x86_64_windows.zip target/x86_64-pc-windows-gnu/release/branch_selector.exe target/x86_64-pc-windows-gnu/release/clean_branches.exe
+zip -j build/git-tools-${VERSION}-${TARGET}.zip target/${TARGET}/release/*

77b3adef9dac08e21400ef5b1968a5ffcc588b3a
Jamie Brynes <jamiebrynes7@gmail.com>
2018-06-13 00:12:22 +0100

    another release fix

--- ci/release.sh
+++ ci/release.sh
@@ -8,6 +8,8 @@ PATCH_VERSION=0
 VERSION="v${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}"
 
 echo "Creating releases for version ${VERSION} and ${TARGET}"
-
 mkdir -p build
+
+# Remove build artifacts and zip
+rm target/${TARGET}/release/*.d target/${TARGET}/release/*.rlib
 zip -j build/git-tools-${VERSION}-${TARGET}.zip target/${TARGET}/release/*
